4efdaff0290111d4f3c15c1aa82eea1e76c306ff
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-11 09:53:40 -0300

    Vista ListaPacientes finalizada

---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,34 @@
+
+
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Mascota: {""}
+          <span className="font-normal normal-case">Ciro</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Propietario: {""}
+          <span className="font-normal normal-case">Ciro</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Email: {""}
+          <span className="font-normal normal-case">Ciro</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Fecha Alta: {""}
+          <span className="font-normal normal-case">Ciro</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Síntomas: {""}
+          <span className="font-normal normal-case">Ciro</span>
+        </p>
+      </div>
+  )
+}
+
+export default Pacientes
