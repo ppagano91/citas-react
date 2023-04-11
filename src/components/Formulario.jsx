d56aae33b8acc2ceb55aa07dcd11b0b5cef23b7a
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-11 14:17:29 -0300

    Agrego useState al Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from "react"
 
 const Formulario = () => {
+  // Hooks
+  const [mascota, setMascota] = useState("")
+  const [propietario, setPropietario] = useState("")
+  const [email, setEmail] = useState("")
+  const [fecha, setFecha] = useState("")
+  const [sintomas, setSintomas] = useState("")
+
+  const handleSUmbit = (e)=> {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Añade Paciente y {""}
         <span className="text-indigo-600 font-bold"> Adminístralos</span>
       </p>
-      <form action="" className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+      <form 
+        action="" 
+        className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'
+        onSubmit={(e)=>handleSUmbit(e)}
+        >
         <div className='mb-5'>
           <label htmlFor="mascota" className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
           <input
@@ -16,6 +32,8 @@ const Formulario = () => {
             type="text"
             placeholder='Nombre de la Mascota'
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={mascota}
+            onChange={(e)=>setMascota(e.target.value)}
             />
         </div>
 
@@ -26,6 +44,8 @@ const Formulario = () => {
             type="text"
             placeholder='Nombre del Propietario'
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange={(e)=>setPropietario(e.target.value)}
             />
         </div>
 
@@ -36,25 +56,35 @@ const Formulario = () => {
             type="email"
             placeholder='Email de Contacto'
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             />
         </div>
 
         <div className='mb-5'>
-          <label htmlFor="alta" className='block text-gray-700 uppercase font-bold'>Alta</label>
+          <label htmlFor="alta" className='block text-gray-700 uppercase font-bold'>
+            Alta
+          </label>
           <input
             id='alta'
             type="date"
             className="border-2 w-full p-2 mt-2"
+            value={fecha}
+            onChange={(e)=>setFecha(e.target.value)}
             />
         </div>
 
         <div className='mb-5'>
-          <label htmlFor="sintomas" className='block text-gray-700 uppercase font-bold'>Síntomas</label>
+          <label htmlFor="sintomas" className='block text-gray-700 uppercase font-bold'>
+            Síntomas
+          </label>
           <textarea
             id='sintomas'
             type="date"
             placeholder='Describe los sintomas'
             className="border-2 w-full p-2 mt-2"
+            value={sintomas}
+            onChange={(e)=>setSintomas(e.target.value)}
             />
         </div>
 
--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -2,7 +2,7 @@
 
 const Pacientes = () => {
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Mascota: {""}
           <span className="font-normal normal-case">Ciro</span>
@@ -10,22 +10,22 @@ const Pacientes = () => {
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Propietario: {""}
-          <span className="font-normal normal-case">Ciro</span>
+          <span className="font-normal normal-case">Patricio</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Email: {""}
-          <span className="font-normal normal-case">Ciro</span>
+          <span className="font-normal normal-case">patricio.pagano@geosystems.com.ar</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Fecha Alta: {""}
-          <span className="font-normal normal-case">Ciro</span>
+          <span className="font-normal normal-case">10/07/1991</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Síntomas: {""}
-          <span className="font-normal normal-case">Ciro</span>
+          <span className="font-normal normal-case">Todo bien</span>
         </p>
       </div>
   )
