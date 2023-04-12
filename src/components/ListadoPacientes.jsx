c66fdd2c9a4fcf32d5a9e4ebfdfecaa2bc5f91f8
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-12 09:35:43 -0300

    Mostando texto de forma condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,18 +1,32 @@
 import Paciente from "./Paciente"
 
 const ListadoPacientes = ({pacientes}) => {
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
-
-      {pacientes.map((paciente)=>{
-        return <Paciente key={paciente.id} paciente={paciente}/>
-
-      })}
+      {pacientes && pacientes.length? (
+          <>
+            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Administra tus {""}
+              <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+            </p>
+      
+            {pacientes.map((paciente)=>{
+              return <Paciente key={paciente.id} paciente={paciente}/>
+              
+            })}
+          </>
+      ):
+          <>
+            <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Los pacientes agregados {""}
+              <span className="text-indigo-600 font-bold">aparecerán aquí abajo</span>
+            </p>
+          </>
+      }
+      
       
     </div>
     
